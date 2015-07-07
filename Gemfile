0d863cfd046d5cddea7a394131921589e5a6d554
Michael Dorrance <dorrancemd@gmail.com>
2015-07-07 11:44:56 -0600

    adds update and delete fuctionality

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'sinatra', require: 'sinatra/base'
+gem 'sinatra', require: 'sinatra/base' # /base is for modular style of Sinatra
 gem 'shotgun'
--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,28 +2,55 @@ require 'models/task_manager'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
+  # set is a method in Sinatra and required
+  set :method_override, true # this allows us to use _method in the form
 
   get '/' do
     'hello, world!'
     erb :dashboard
   end
 
-  get '/tasks' do
+  get '/tasks' do #READ
     @tasks = TaskManager.all
     erb :index
   end
 
-  get '/tasks/new' do
+  get '/tasks/new' do #CREATE - displays a form
     erb :new
   end
 
-  post '/tasks' do
+  post '/tasks' do #CREATE - takes the data from form
     TaskManager.create(params[:task])
     redirect '/tasks'
   end
 
-  get '/tasks/:id' do |id|
+  get '/tasks/:id' do |id| #READ
     @task = TaskManager.find(id.to_i)
     erb :show
   end
+
+  # get '/tasks/:id' do 
+  #   @task = TaskManager.find(params[:id].to_i)
+  #   erb :show
+  # end   THIS is an alternate the the get above
+
+  get '/tasks/:id/edit' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    TaskManager.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect "/tasks"
+  end
+
+  not_found do
+    erb :error
+  end
+
 end
